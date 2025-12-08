9964367daf2bc3da728e5128b115abd31dc2696d
Rakesh Roushan <rakeshroushan620570@gmail.com>
2025-12-08 21:09:37 +0530

    Initial commit: portfolio setup

---
+++ src/data/constants.js
@@ -0,0 +1,420 @@
+import { Instagram } from "@mui/icons-material";
+
+export const Bio = {
+  name: "Rakesh Roushan",
+  roles: ["Java Full Stack Developer", "Programmer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/rakeshroushan6205/code.git",
+  resume:
+    "https://drive.google.com/file/d/13w9W8hJU_rxlAXZshuACeD2-9frYKp1k/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rakeshro-java-dev",
+  email:
+    "https://mail.google.com/mail/?view=cm&fs=1&to=rakeshroushan620570@gmail.com",
+  insta: "https://www.instagram.com/rakeshroushan391/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "Spring Boot",
+        image: "https://miro.medium.com/1*os-kEY6vkmhbx3TEznlOng.png",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Spring Framework",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeFkEoxqx0aROV16RLFKyz6QErGIQb-nY_jA&s",
+      },
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "H2 Databases",
+        image: "https://www.javatips.net/static/images/h2.png",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud",
+        image:
+          "https://algotrading101.com/learn/wp-content/uploads/2022/09/Google-Cloud-Logo-Lockup-MAIN-png-1.webp",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Jenkins",
+        image:
+          "https://toppng.com/uploads/preview/jenkins-logo-11609365847mufysaivph.png",
+      },
+      {
+        name: "Nginx",
+        image: "https://download.logo.wine/logo/Nginx/Nginx-Logo.wine.png",
+      },
+      {
+        name: "Grafana",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Grafana_logo.svg/1200px-Grafana_logo.svg.png",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/00/Kubernetes_%28container_engine%29.png",
+      },
+    ],
+  },
+
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/683px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "C",
+        image:
+          "https://e7.pngegg.com/pngimages/465/779/png-clipart-blue-and-white-c-logo-the-c-programming-language-computer-programming-computer-icons-programmer-blue-angle.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image: "https://www.rlogical.com/wp-content/uploads/2023/03/31d.webp",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "intellij",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8fbwusxe4WMoRjHlWi_5QYN5VlDqZmwXDgQ&s",
+      },
+      {
+        name: "eclipse",
+        image: "https://avatars.githubusercontent.com/u/176413019?s=200&v=4",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTOndlNFKOUXtQA4DkTirUbZoCSOirFo0ZKbNT2EwY48PPvZjTf_wsS9jFC18ZzZ2HYOw&usqp=CAU",
+      },
+
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+
+      {
+        name: "Vercel",
+        image:
+          "https://x5h8w2v3.delivery.rocketcdn.me/wp-content/uploads/2024/03/Logo-Vercel.png",
+      },
+      {
+        name: "Adobe Photoshop",
+        image:
+          "https://www.greaterthangatsby.com/wp-content/uploads/2016/06/ps2.jpg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://images.jdmagicbox.com/v2/comp/bangalore/s4/080pxx80.xx80.220314014338.t9s4/catalogue/tap-academy-pvt-ltd-sbi-bank-colony-bangalore-tutorials-uj7mpei0gw.jpg",
+    role: "Training program",
+    company: "TAP Academy",
+    date: "June 2025 - Nov 2025",
+    desc: "I completed a Java Full Stack Developer training program, gaining hands‑on expertise in both frontend and backend technologies. This included professional coaching with practical training in Java, Spring Boot, Hibernate, REST APIs, React, and MySQL, where I developed real‑world projects demonstrating end‑to‑end application design, deployment, and integration.",
+    skills: [
+      "Java",
+      "Programming",
+      "Hibernate",
+      "Spring Boot",
+      "JDBC",
+      "Servlets/J2ee",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "",
+  },
+
+  {
+    id: 1,
+    img: "https://images.credly.com/images/0bf0f2da-a699-4c82-82e2-56dcf1f2e1c7/image.png",
+    role: "Certificate",
+    company: "Coursera Google Career",
+    date: "Feb 2024 - March 2024",
+    desc: "Completed the Google Cybersecurity Professional Certificate, gaining hands‑on expertise in Python, Linux, SQL, SIEM tools, and IDS. Trained to identify and mitigate risks, detect and respond to threats, and automate security tasks. Developed real‑world projects across eight specialized courses, preparing for entry‑level cybersecurity roles.",
+    skills: [
+      "Foundations of Cybersecurity",
+      "Risk Management",
+      "Network Security",
+      "Threats & Vulnerabilities",
+      "Detection & Response",
+    ],
+    doc: "",
+  },
+
+  {
+    id: 2,
+    img: "https://yt3.googleusercontent.com/Pbc4qi4MSNbjqyu4Lh3Qrt99yRPGwF6WqzSKUHYuWhRvxra6E4WidZq_3NOuKwl2YQil9MrEKA=s900-c-k-c0x00ffffff-no-rj",
+    role: "Artificial Intelligence and Machine Learning Internship",
+    company: "YBI Foundation",
+    date: "Dec 2024 – Feb 2025",
+    desc: "Completed an Artificial Intelligence and Machine Learning Internship, showcasing strong dedication, professionalism, and hands‑on project experience in applying AI/ML concepts.",
+    skills: [
+      "Machine Learning algorithms",
+      "Data preprocessing",
+      "Python libraries",
+      "Neural networks and deep learning basics",
+      "Natural Language Processing",
+    ],
+    doc: "",
+  },
+
+  {
+    id: 3,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Salesforce.com_logo.svg/1200px-Salesforce.com_logo.svg.png",
+    role: "Salesforce Developer Virtual Internship",
+    company: "Salesforce",
+    date: "Dec 2023 – Jan 2024",
+    desc: "During my Salesforce Developer Virtual Internship, I gained hands‑on experience with the Salesforce platform and its core development tools. I built a strong foundation in Salesforce fundamentals, explored relationship management and process automation, and applied Apex programming for custom solutions. The internship also enhanced my skills in testing and debugging.",
+    skills: [
+      "Salesforce Fundamentals",
+      "Relationship & Process Automation",
+      "Apex Programming",
+      "Testing & Debugging",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/3/32/Gaya_College_of_Engineering_logo.jpg",
+    school: "Gaya College Of Engineering , Gaya",
+    date: "Dec 2021 - June 2025",
+    grade: "7.84 CGPA",
+    desc: "I have completed my B.Tech in Computer Science & Engineering from Gaya College of Engineering with a CGPA of 7.84 .  I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I built a strong academic foundation while also developing essential professional skills. I gained in-depth knowledge of my field through coursework and projects, and sharpened my ability to apply concepts in practical scenarios. I also improved my communication and leadership abilities by participating in group activities and presentations.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://media.collegedekho.com/media/img/institute/logo/1437217984.jpg",
+    school: "Nalanda College",
+    date: "Apr 2019 - Apr 2021",
+    grade: "84.4%",
+    desc: "I completed my class 12 high school education at  Nalanda College, Nalanda, I pursued my higher secondary education in the Science stream",
+    degree: "BSEB(XII), Academic Stream: Science",
+  },
+  {
+    id: 2,
+    img: "https://schools.olympiadsuccess.com/assets/images/school_logo/97871_logo.jpg",
+    school: "Sabarmati Gyan Niketan",
+    date: "Apr 2017 - Apr 2019",
+    grade: "83.2%",
+    desc: "I completed my class 10 education at Sabarmati Gyan Niketan, Nalanda.",
+    degree: "CBSE(X), Academic Stream: Science ",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Employee Management System",
+    date: "Jan 2024 - March 2024",
+    description:
+      "The primary purpose of developing the Employee Management System is to create a centralized, efficient, and scalable platform for managing employee and department information. By integrating a React frontend with a Spring Boot backend",
+    image: "/planet/ems.png",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Hibernate",
+      "mySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "",
+  },
+  {
+    id: 1,
+    title: "TrendHive-Ecommerce website",
+    date: "June 2025 - Sep 2025",
+    description:
+      "TrendHive is a modern, full‑stack e‑commerce platform designed to deliver a seamless online shopping experience. It combines a visually appealing React frontend with a powerful Spring Boot backend, ensuring both usability and performance for customers and administrators.",
+    image: "/planet/ecommerce.png",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Hibernate",
+      "mySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+
+  {
+    id: 2,
+    title: "SparkCart",
+    date: "Oct 2025 - Dec 2025",
+    description:
+      "SparkCart is a modern, full‑stack grocery shopping platform designed to make everyday essentials accessible, convenient, and efficient. It combines a sleek React frontend with a reliable Spring Boot backend, offering customers a seamless experience from browsing products to completing purchases.",
+    image: "/planet/grocery.png",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Hibernate",
+      "mySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "EMedicareApp",
+    date: "May 2024 - June 2024",
+    description:
+      "The purpose of EMedicareApp is to provide a digital healthcare marketplace where users can easily find and purchase medicines, while pharmacies and administrators can efficiently manage inventory and orders. It demonstrates your ability to build a scalable, secure, and production‑ready full‑stack application tailored to real‑world healthcare needs.",
+    image: "/planet/emdi.png",
+    tags: [
+      "Java",
+      "Spring Boot",
+      "Hibernate",
+      "mySQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+    // member: [
+    //   {
+    //     name: "",
+    //     img: "",
+    //     linkedin: "",
+    //     github: "",
+    //   },
+
+    // ],
+  },
+];
