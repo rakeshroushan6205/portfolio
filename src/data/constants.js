3bcfa4eb9263527caad00975ce8d0ad79b1e18fd
PC <rakeshroushan620570@gmail.com>
2026-08-10 00:22:36 +0530

    Updated portfolio

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/rakeshroushan6205/code.git",
   resume:
-    "https://drive.google.com/file/d/14-mIN39B62AXfQxVGVhkVYMPE_52VtVf/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1Bx7OkVbssqVc2vxWPcANmMwbwq69jfqr/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rakeshro-java-dev",
   email:
     "https://mail.google.com/mail/?view=cm&fs=1&to=rakeshroushan620570@gmail.com",
